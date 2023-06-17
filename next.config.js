3e9b7e8f0b39d9e4af175650db86747b29d3ba2d
adamfrsd-02 <adamfrdsid@gmail.com>
2023-06-17 14:19:24 +0700

    commit

--- next.config.js
+++ next.config.js
@@ -9,9 +9,6 @@ const nextConfig = {
             }
         ]
     },
-    experimental:{
-        forceSwcTransforms: true
-    },
     reactStrictMode: true,
     images: {
         domains: ['hidropol-frontend.vercel.app']
